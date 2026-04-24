9bd9cb57069826f58307ace080037b834ac57adc
Ege Bilir <ege.bilir@gmail.com>
2026-04-25 00:10:08 +0300

    Initial Commit

---
+++ KeyboardCleaner/KeyboardCleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardCleanerApp.swift
+//  KeyboardCleaner
+//
+//  Created by Ege Bilir on 25.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct KeyboardCleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
